432ff46ecfd83ee54382fc4a4c907dc794dad521
Lee Ourand <lee.ourand@vts.com>
2022-09-19 21:55:29 -0400

    Fixes bug where relative paths are unresolved

--- lib/favicon_extractor/extracts_favicons_from_html.rb
+++ lib/favicon_extractor/extracts_favicons_from_html.rb
@@ -2,11 +2,12 @@ module FaviconExtractor
   class ExtractsFaviconsFromHtml
     class << self
       def extract(html, url)
+        @url = url
         html.scan(/<link.*icon.*>/).flatten.map do |str|
           str.match(/href="(.*?)"/).captures.first
         end
           .map { |str| handle_relative_paths(str) }
-          .append(default_favicon_for(url))
+          .append(default_favicon)
       end
 
       private
@@ -15,7 +16,7 @@ module FaviconExtractor
 
       def handle_relative_paths(str)
         if str.start_with?("/")
-          uri = URI(url)
+          uri = URI(@url)
           uri.path = str
           uri.to_s
         else
@@ -23,8 +24,8 @@ module FaviconExtractor
         end
       end
 
-      def default_favicon_for(url)
-        uri = URI(url)
+      def default_favicon
+        uri = URI(@url)
         uri.path = DEFAULT_FAVICON_PATH
         uri.to_s
       end
